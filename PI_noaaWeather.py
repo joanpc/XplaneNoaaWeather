2758ecfb0f96b4ee99957431bd0c3230c71f09c4
Joan <joan@joan.lan>
2012-12-07 01:13:38 +0100

    release

--- PI_noaaWeather.py
+++ PI_noaaWeather.py
@@ -29,7 +29,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 '''
-__VERSION__ = '1.4.2'
+__VERSION__ = '1.4.3'
 
 #Python includes
 from datetime import datetime, timedelta
