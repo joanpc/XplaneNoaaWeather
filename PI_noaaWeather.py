c61d93bb89b11d1197bb983d4948aa19d715b030
joan <joan@joans-MBP.lan>
2015-01-30 19:49:25 -0500

    1.8 release

--- PI_noaaWeather.py
+++ PI_noaaWeather.py
@@ -26,7 +26,7 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 '''
-__VERSION__ = '1.7'
+__VERSION__ = '1.8'
 
 #Python includes
 from datetime import datetime, timedelta
