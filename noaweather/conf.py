36ff8aa39261dfe78f5cbaeb9493b7c5d26da9a6
joan <joan@joans-MBP.lan>
2015-02-20 19:39:11 -0500

    Beta 2

--- noaweather/conf.py
+++ noaweather/conf.py
@@ -8,7 +8,7 @@ class Conf:
     Configuration variables
     '''
     syspath, dirsep = '', os.sep
-    __VERSION__ = '2.0_beta1.1'
+    __VERSION__ = '2.0_beta2'
     
     def __init__(self, syspath):
         # Inits conf
