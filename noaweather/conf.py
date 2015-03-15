dc8fcb58b6eed568d97e6cc254a165ac6026392d
joan <joanpc@gmail.com>
2015-03-15 15:27:38 -0500

    2.0rc4d

--- noaweather/conf.py
+++ noaweather/conf.py
@@ -18,7 +18,7 @@ class Conf:
     Configuration variables
     '''
     syspath, dirsep = '', os.sep
-    __VERSION__ = '2.0rc4c'
+    __VERSION__ = '2.0rc4d'
     
     def __init__(self, syspath):
         # Inits conf
