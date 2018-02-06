6a28dc6c1cddb69e81e20b63530711288e2bed2f
joan <joan@Cristinas-Air.lan>
2018-02-06 16:42:17 -0500

    2.4.3

--- noaweather/conf.py
+++ noaweather/conf.py
@@ -20,7 +20,7 @@ class Conf:
     syspath, dirsep = '', os.sep
     printableChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ '
 
-    __VERSION__ = '2.4.2'
+    __VERSION__ = '2.4.3'
 
     def __init__(self, syspath):
         # Inits conf
